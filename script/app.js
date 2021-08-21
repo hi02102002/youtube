b741c338e75bf3a27f321c34288207367022a18a
Võ Hoàng Huy <84460646+hi02102002@users.noreply.github.com>
2021-08-21 23:56:53 +0700

    miss responsive

--- script/app.js
+++ script/app.js
@@ -2,11 +2,18 @@
 //show hide sidebar
 const toggleBtn = document.querySelector(".header__toggle");
 const sidebar = document.querySelector(".sidebar");
+const sidebarLinks = document.querySelectorAll(".sidebar__link");
+const content = document.querySelector(".content");
+const tag = document.querySelector(".tag");
+window.addEventListener("load", function () {
+   content.style.marginLeft = `${sidebar.getBoundingClientRect().width}px`;
+});
+
 toggleBtn.addEventListener("click", function (e) {
    sidebar.classList.toggle("hidden");
+   content.style.marginLeft = `${sidebar.getBoundingClientRect().width}px`;
 });
 // nav link//
-const sidebarLinks = document.querySelectorAll(".sidebar__link");
 
 sidebarLinks.forEach((link) => {
    link.addEventListener("click", function (e) {
@@ -18,4 +25,3 @@ sidebarLinks.forEach((link) => {
       link.classList.add("active");
    });
 });
-//
